b622dca81627d0b02804ea2be16809cb487825b9
Martin Gaido <martin.gaido@gmail.com>
2020-07-26 11:42:36 -0300

    Includes production build (just for testing purposes)

---
+++ build/precache-manifest.1573c1d99becb15d519dade288d45472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6020c79903ccea544c0db9225a84531",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ba410bf93b387ed97eb",
+    "url": "/static/css/main.5bc859a5.chunk.css"
+  },
+  {
+    "revision": "505f14a90b8b1958aec3",
+    "url": "/static/js/2.8181496f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8181496f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ba410bf93b387ed97eb",
+    "url": "/static/js/main.cc0457eb.chunk.js"
+  },
+  {
+    "revision": "bf250259d9fd61dc7dbe",
+    "url": "/static/js/runtime-main.ecec8c93.js"
+  }
+]);
